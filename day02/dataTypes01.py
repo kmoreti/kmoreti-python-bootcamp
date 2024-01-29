ba19002635c1aca63ae1d0d019fc3a4e0bcac99c
kmoreti <kleber.moreti.camargo@gmail.com>
2024-01-29 23:05:26 +0000

    Data types

---
+++ day02/dataTypes01.py
@@ -0,0 +1,19 @@
+# Data types
+
+# String
+print("Hello"[4])
+
+print("123" + "345")
+
+# Integer
+
+print(123 + 345)
+
+# Float
+
+print(3.14159)
+
+# Boolean
+
+
+
---
+++ day02/dataTypes02.py
@@ -0,0 +1,18 @@
+# num_char = len(input("What is your name?"))
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
+#
+# print(type(num_char))
+
+
+a = 123
+print(type(a))
+
+a = str(123)
+print(type(a))
+
+a = float(123)
+print(type(a))
+
+print(70 + float("100.5"))
+print(str(70) + str(100))
