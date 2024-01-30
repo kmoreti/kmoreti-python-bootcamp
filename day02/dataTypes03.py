fa1a47921762fe03d4f20cde3fbb4f458a4e2468
kmoreti <kleber.moreti.camargo@gmail.com>
2024-01-30 22:22:10 +0000

    Data types

--- day02/dataTypes03.py
+++ day02/dataTypes03.py
@@ -1,8 +1,8 @@
-3 + 5
-7 - 4
-3 * 2
-6 / 3
-2 ** 3
+print(3 + 5)
+print(7 - 4)
+print(3 * 2)
+print(6 / 3)
+print(2 ** 3)
 print(type(6 / 3))
 
 print(2 ** 3)
---
+++ day02/dataTypes04.py
@@ -0,0 +1,26 @@
+print(8 / 3)
+print(int(8 / 3))
+print(round(8 / 3))
+print(round(8 / 3, 2))
+print(round(2.6666666666, 2))
+# floor division
+print(8 // 3)
+print(type(8 // 3))
+print(type(8 / 3))
+print(type(4 / 2))
+
+
+result = 4 / 2
+result /= 2
+print(result)
+
+score = 0
+# User scores a point
+score += 1
+height = 1.8
+isWinning = True
+print(score)
+
+print("your scored is " + str(score))
+# f-String
+print(f"your scored is {score}, your height is {height}, you are winning is {isWinning}")
