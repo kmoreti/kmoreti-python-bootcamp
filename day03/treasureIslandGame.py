3ea7cdc871e74514edbd16a8667c1a86fca628e7
kmoreti <kleber.moreti.camargo@gmail.com>
2024-02-01 22:54:18 +0000

    Treasure Island

--- day03/treasureIslandGame.py
+++ day03/treasureIslandGame.py
@@ -26,24 +26,20 @@ print("Your mission is to find the treasure.")
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 👇
-direction = input("Left or right? ").lower()
+direction = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
 if direction == "left":
-    action = input("Swin of wait? ").lower()
+    action = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
     if action == "wait":
-        door = input("Which door? (red|yellow|blue) ").lower()
+        door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
         if door == "yellow":
-            print("You Win!")
+            print("You found the treasure! You Win!")
         elif door == "blue":
-            print("Eaten by beasts.")
-            print("Game Over.")
+            print("You enter a room of beasts. Game Over.")
         elif door == "red":
-            print("Burned by fire.")
-            print("Game Over.")
+            print("It's a room full of fire. Game Over.")
         else:
             print("Game Over.")
     else:
-        print("Attacked by trout.")
-        print("Game Over.")
+        print("You get attacked by an angry trout. Game Over.")
 else:
-    print("Fall into a hole.")
-    print("Game Over.")
+    print("You fell into a hole. Game Over.")
